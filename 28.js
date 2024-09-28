b38f9bb3b9afec778f55436d6f0e4c209264e53b
ShanzayEman <shanzayeman0@gmail.com>
2024-09-28 22:07:22 +0500

    Add files via upload

---
+++ 28.js
@@ -0,0 +1,5 @@
+const fruits=["Orange","Grapes","Banana","Apple"];
+let chand =fruits.at(3);
+document.getElementById("demo").innerHTML="My favourite Fruit is:"+ chand;
+const fruits1=["Orange","Grapes","Banana","Apple"];
+document.getElementById("demo1").innerHTML="My favourite Fruit is:"+ fruits.join("^");
